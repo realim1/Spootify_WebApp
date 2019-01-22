adcddacf4bf01af167f0b2ca167af4f8081f2c0f
Markus <35947766+realim1@users.noreply.github.com>
2019-01-21 21:54:55 -0800

    Added functionality to music progress bar and volume level

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,60 @@
 var currentPlayList = [];
 var audioElement;
+var mouseDown = false;
+var savedVolume;
 
+
+function formatTime(seconds) {
+
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60);
+    var seconds = time - (minutes * 60);
+
+    var extraZero = (seconds < 10) ? "0" : "";
+
+    return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio){
+    $(".progressTime.current").text(formatTime(audio.currentTime));
+    $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+    var progress = audio.currentTime / audio.duration * 100;
+
+    $(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio){
+    var volume = audio.volume * 100;
+    $(".volumeBar .progress").css("width", volume + "%");
+}
 
 function Audio() {
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
-    //function of class Audio
+
+    //EventListeners of class Audio
+    this.audio.addEventListener("canplay", function(){
+        //'this' refers to the object that the event was called on
+        // equvialent to this.audio.duration
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+        updateVolumeProgressBar(this);
+    });
+
+    this.audio.addEventListener("timeupdate", function(){
+        if(this.duration){
+            updateTimeProgressBar(this);
+        }
+    });
+
+    this.audio.addEventListener("volumechange", function(){
+        updateVolumeProgressBar(this);
+    });
+
+    //Functions of class Audio
     this.setTrack = function(track) {
         this.currentlyPlaying = track;
         this.audio.src = track.path;
@@ -21,5 +68,18 @@ function Audio() {
         this.audio.pause();
     }
 
+    this.mute = function(){
+        savedVolume = this.audio.volume;
+        this.audio.volume = 0;
+    }
+
+    this.unmute = function(){
+        this.audio.volume = savedVolume;
+    }
+
+
+    this.setTime = function(seconds){
+        this.audio.currentTime = seconds;
+    }
 
 }
--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -15,38 +15,93 @@
 <script>
 
     $(document).ready(function(){
-
         currentPlayList = <?php echo $jsonArray; ?>;
         audioElement = new Audio();
-        setTrack(currentPlayList[0], currentPlayList, false);    
+        setTrack(currentPlayList[0], currentPlayList, false); 
+        updateVolumeProgressBar(audioElement.audio);
+        
+        $(".playbackBar .progressBar").mousedown(function(){
+            mouseDown = true;
+        });
+
+        $(".playbackBar .progressBar").mouseup(function(e){
+            timeFromOffset(e, this);
+        });
+
+        $(".playbackBar .progressBar").mousemove(function(e){
+            if(mouseDown == true){
+                timeFromOffset(e, this);
+            }
+        });
+
+
+        $(".volumeBar .progressBar").mousedown(function(){
+            mouseDown = true;
+        });
+
+        $(".volumeBar .progressBar").mouseup(function(e){
+            var percentage = e.offsetX / $(this).width();
+            if(percentage >= 0 && percentage <= 1){
+                audioElement.audio.volume = percentage;
+                if(percentage == 0){
+                    $(".controlButton.volume").hide();
+                    $(".controlButton.novolume").show();
+                }
+                else{
+                    $(".controlButton.novolume").hide();
+                    $(".controlButton.volume").show();
+                }
+            }
+        });
+
+        $(".volumeBar .progressBar").mousemove(function(e){
+            if(mouseDown == true){
+                var percentage = e.offsetX / $(this).width();
+                if(percentage >= 0 && percentage <= 1){
+                    audioElement.audio.volume = percentage;
+                    if(percentage == 0){
+                        $(".controlButton.volume").hide();
+                        $(".controlButton.novolume").show();
+                    }
+                    else{
+                        $(".controlButton.novolume").hide();
+                        $(".controlButton.volume").show();
+                    }
+                }
+            }
+        });
+
+        $(document).mouseup(function(){
+            mouseDown = false;
+        });
+
     });
+
+function timeFromOffset(mouse, progressBar){
+    var percentage = mouse.offsetX / $(progressBar).width() * 100;
+    var seconds = audioElement.audio.duration * (percentage / 100);
+
+    audioElement.setTime(seconds);
+}
 
 function setTrack(trackId, newPlayList, play){
 
     $.post("includes/handlers/ajax/getSongJson.php", {songId: trackId}, function(data) {
 
         var track = JSON.parse(data);
-        
         $(".trackName span").text(track.title);
 
         $.post("includes/handlers/ajax/getArtistJson.php", {artistId: track.artist}, function(data) {
-
             var artist = JSON.parse(data);
-
             $(".artistName span").text(artist.name);
-
         });
 
         $.post("includes/handlers/ajax/getAlbumJson.php", {albumId: track.album}, function(data) {
-
             var album = JSON.parse(data);
-
             $(".albumLink img").attr("src", album.artworkPath);
-
         });
 
         audioElement.setTrack(track);
-        playSong();
     });
     
     if(play == true) {
@@ -72,6 +127,18 @@ function pauseSong(){
     $(".controlButton.pause").hide();
     $(".controlButton.play").show();
     audioElement.pause();
+}
+
+function muteSong(){
+    $(".controlButton.volume").hide();
+    $(".controlButton.novolume").show();
+    audioElement.mute();
+}
+
+function unmuteSong(){
+    $(".controlButton.novolume").hide();
+    $(".controlButton.volume").show();
+    audioElement.unmute();
 }
 
 
@@ -140,9 +207,14 @@ function pauseSong(){
 
     <div id="nowPlayingRight">
         <div class="volumeBar">
-            <button class="controlButton volume" title="Volume button">
+            <button class="controlButton volume" title="Volume button" onclick="muteSong()">
                 <img src="assets/images/icons/volume.png" alt="Volume">
             </button>
+
+            <button class="controlButton novolume" title="Volume button" style="display: none;" onclick="unmuteSong()">
+                <img src="assets/images/icons/muted.png" alt="Volume">
+            </button>
+
             <div class="progressBar">
                 <div class="progressBarBackground">
                     <div class="progress"></div>                                    
